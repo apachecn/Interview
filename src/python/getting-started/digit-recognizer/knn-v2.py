dc1643d2da61254017babec019cd8e892e513269
hduyyg <2632158294@qq.com>
2018-03-20 10:12:52 +0800

    add knn-v2,solved with pca+knn,score=0.975

---
+++ src/python/getting-started/digit-recognizer/knn-v2.py
@@ -0,0 +1,22 @@
+import functions
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def get_result():
+    train_data, train_label, test_data = functions.read_data_from_csv()
+
+    pca_model = PCA(n_components=35)
+    pca_model.fit(train_data)
+    new_train_data = pca_model.transform(train_data)
+    new_test_data = pca_model.transform(test_data)
+
+    knn_model = KNeighborsClassifier(n_neighbors=4, weights='distance')        
+    knn_model.fit(new_train_data, train_label)
+    test_label = knn_model.predict(new_test_data)
+    return test_label
+
+
+if __name__ == '__main__':
+    result = get_result()
+    functions.save_result(result)
