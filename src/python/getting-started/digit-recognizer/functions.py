29d65dbd68970f5e3728c6a5e236fced49a13508
hduyyg <2632158294@qq.com>
2018-03-28 14:17:01 +0800

    add shrink_img() in functions.py to resize img

--- src/python/getting-started/digit-recognizer/functions.py
+++ src/python/getting-started/digit-recognizer/functions.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from scipy import misc
 
-
 def read_data_from_csv():
     data1 = pd.read_csv('train.csv')
     train_data = data1.values[0:, 1:]
@@ -13,14 +12,12 @@ def read_data_from_csv():
     
     return train_data, train_label, test_data
 
-
 def save_result(data):
     csv_path, info = 'result.csv', {}
     info['ImageId'] = [i for i in range(1, len(data) + 1)]
     info['Label'] = data
     data_frame = pd.DataFrame(info)
     data_frame.to_csv(csv_path, index=False, sep=',')
-
 
 def shrink_img(data, rate=0.5):
     tmp = [None for _ in data]
