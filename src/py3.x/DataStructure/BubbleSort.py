43d5727f34719ed81506e22afcbe3a15398cee0c
jiangzhonglian <jiang-s@163.com>
2020-04-13 22:42:00 +0800

    更新冒泡排序

--- src/py3.x/DataStructure/BubbleSort.py
+++ src/py3.x/DataStructure/BubbleSort.py
@@ -2,19 +2,20 @@
 
 
 # 冒泡排序
-# 1. 外层循环负责帮忙递减内存循环的次数【1, len-1】
-# 2. 内层循环负责前后两两比较, index 的取值范围【0, len-2】 len-1-i 次，求最大值放到最后
+# 1. 外层循环负责循环的次数，依次递减到1就停止(1个数不存在下一个值)
+# 2. 内层循环负责前后两两比较, 判断是否需要交换位置，然后移动判断
 def bubble_sort(nums):
-    # [1, len-1]
-    for i in range(1, len(nums)-1):
-        # [0, len-1-i]
-        for j in range(len(nums)-i):  # ｊ为列表下标
-            if nums[j] > nums[j+1]:
-                nums[j], nums[j+1] = nums[j+1], nums[j]
-    return nums
+    # 索引长度，到倒数第二个就行
+    index = len(nums) - 2
+    while index:
+        # 第一个数字，和后面每一个数字进行对比，找出最大值，放到最后！！
+        for i in range(index):
+            if nums[i] > nums[i+1]:
+                    nums[i], nums[i+1] = nums[i+1], nums[i]
+        index -= 1
 
 
 if __name__ == "__main__":
-    nums = [2, 6, 8, 5, 1, 4, 9, 3, 7]
+    nums = [3, 6, 8, 5, 2, 4, 9, 1, 7]
     bubble_sort(nums)
     print('result:', nums)
